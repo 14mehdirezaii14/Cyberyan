cfdef1c290e0f0eb05d27fb7ffca3536cb7ebea4
99mehdirezaii99@gmail.com <99mehdirezaii99@gmail.com>
2026-09-05 19:10:56 +0330

    chore: enable cors for local port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,13 @@ async function bootstrap() {
 
   SwaggerModule.setup('/api', app, document);
 
-  await app.listen(3000);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
